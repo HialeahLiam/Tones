8cd03aaadd333ded71c8ee68189beb125306b43c
Liam Idrovo <liamidrovo@gmail.com>
2022-01-23 22:42:53 -0500

    updated heroku URL

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const app = express();
 
-const PORT = process.env.NODE_ENV == "production" ? "https://tonesapp.herokuapp.com" : 8001;
+const PORT = process.env.NODE_ENV == "production" ? "https://git.heroku.com/tonesapp.git" : 8001;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
